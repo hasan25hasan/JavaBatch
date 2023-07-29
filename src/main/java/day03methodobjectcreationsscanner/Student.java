7521d783c5bff0779e95f4ff69bdf967434469e9
hasan25hasan <hb25315312@gmail.com>
2023-07-29 17:52:42 +0300

    new1

---
+++ src/main/java/day03methodobjectcreationsscanner/Student.java
@@ -0,0 +1,30 @@
+package day03methodobjectcreationsscanner;
+
+public class Student {
+    //Variable (==>pasif ozellikler)
+    public String name = "Ali Can";
+    public byte grade = 8;
+    public String adress = "Ankara";
+
+
+    //method (==>aktif ozellikler)
+
+    public void study() {
+
+        System.out.println("Gunluk Tekrarlarini asla ihmal etmez");
+    }
+
+    public void feed() {
+        System.out.println("Saglikli beslenir");
+
+    }
+
+}
+
+
+
+
+
+
+
+
