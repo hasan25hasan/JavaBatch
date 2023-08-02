7079f6e9d14ade255bbb02f62e4f134b6ba7e384
hasan25hasan <hb25315312@gmail.com>
2023-08-02 13:47:26 +0300

    new1

---
+++ src/main/java/day10ifstataments/IfStatements01.java
@@ -0,0 +1,31 @@
+package day10ifstataments;
+
+import java.util.Scanner;
+
+public class IfStatements01 {
+    public static void main(String[] args) {
+        //Example : Verilen bir sayının pozitif , negatif veya nötr oldugunu kontrol eden kodu yazınız
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lutfen bir sayi giriniz..");
+        int num = input.nextInt();
+
+        if(num>0){
+            System.out.println("Pozitiftir..");
+        }else if(num<0){
+            System.out.println("Negatiftir");
+        }else{
+            System.out.println("Notr");
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
