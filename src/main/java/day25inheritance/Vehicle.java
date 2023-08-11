f0ca46bdb127d675a74a638a550e03761f3e0e01
hasan25hasan <hb25315312@gmail.com>
2023-08-11 23:38:14 +0300

    new1

---
+++ src/main/java/day25inheritance/Vehicle.java
@@ -0,0 +1,31 @@
+package day25inheritance;
+
+
+
+public class Vehicle {
+    public Vehicle(){
+        this(1000);
+        System.out.println("Vehicle 1");
+    }
+    public Vehicle(int price){
+        System.out.println("Vehicle 2");
+    }
+}
+/*
+ 1) Child classtan bir object olusturdugunuzda Constructor en ustteki parent classtan baslatilarak alta dogru calisitirilir
+ 2) Child class taki constructor dan Parent class taki constructor a gidebilmek icin super() kullanilir
+ 3) Parent Class ta birden fazla constructor varsa istenilen constructor super() ifadesinin icine yazilan parametreler yardimi
+  ile secilebilir
+ 4) Ayni class icindeki constructor i secmek icin this() kullanilir ,Ayni class ta birden fazla constructor varsa istenilen
+ constructor this() ifadesinin parantez icine yazilan parametreler yardimi ile secilebilir
+ 5)super() ifadesini yazmak istege baglidir, yazmassaniz da java  sanki super() varmis gibi davaranir,
+ Ama kodunuzun daha okunur kilmak icin yazmaniz tavsiye edilir
+  6) super() ve this() ifadeleri constructor icinde herzaman ilk satirda olmalidir
+  7) Bir constructor icinde super() ve this() ifadeleri sadece bir kere kullanilabilir. (Bakiniz 6.)
+  8)Inhertiance da variableleri ve methodlari çagirmak icin this veya super kullanilir
+    this ayni class icinde variable veya methodlari cagirmak icin kullanilir
+    super parent ayni class icinde variable veya methodlari cagirmak icin kullanilirv
+
+
+
+ */
